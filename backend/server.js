d82e2678b24545469e60e2490b4f99b014995e6d
Ravitheja <gollaravitheja00@gmail.com>
2026-04-23 12:34:33 +0530

    cors error fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://querymind-nltosql.netlify.app', 'http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
